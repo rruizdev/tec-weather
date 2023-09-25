fc161c180f2802f2f47c6ed869de911681ea0015
Roberto Ruiz <rruiz@frba.utn.edu.ar>
2023-09-25 00:07:15 -0300

    Dark mode (no persistent)

--- src/tec-weather/src/App.jsx
+++ src/tec-weather/src/App.jsx
@@ -5,32 +5,35 @@ import Extended from './components/extended/Extended';
 import Location from './components/location/Location';
 import Header from './components/header/Header';
 import Container from 'react-bootstrap/esm/Container';
+import './App.scss'
 
 export default function App() {
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
 
   useEffect(() => {
-      navigator.geolocation.getCurrentPosition(position => {
-          setLatitude(position.coords.latitude);
-          setLongitude(position.coords.longitude);
-      }, () => {
-      });
+    navigator.geolocation.getCurrentPosition(position => {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    }, () => {
+    });
   }, []);
 
   return (
     <>
-      <Header/>
-      <Container>
-        <Routes>
-          <Route>
-            <Route path={'today'} element={ <Today latitude={latitude} longitude={longitude} /> } />
-            <Route path={'extended'} element={ <Extended latitude={latitude} longitude={longitude} /> } />
-            <Route path={'locations'} element={ <Location /> } />
-            <Route path={'*'} element={ <Navigate to={'/today'} /> } />
-          </Route>
-        </Routes>
-      </Container>
+      <Header />
+      <div className='app'>
+        <Container>
+          <Routes>
+            <Route>
+              <Route path={'today'} element={<Today latitude={latitude} longitude={longitude} />} />
+              <Route path={'extended'} element={<Extended latitude={latitude} longitude={longitude} />} />
+              <Route path={'locations'} element={<Location />} />
+              <Route path={'*'} element={<Navigate to={'/today'} />} />
+            </Route>
+          </Routes>
+        </Container>
+      </div>
     </>
   );
 };
--- src/tec-weather/src/components/header/Header.jsx
+++ src/tec-weather/src/components/header/Header.jsx
@@ -1,18 +1,37 @@
+import { useEffect, useState } from 'react';
+import { Moon, Sun } from 'react-bootstrap-icons';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './Header.scss';
 
 export default function Header() {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = (event) => {
+        event.preventDefault();
+        setDarkMode(!darkMode);
+    }
+
+    useEffect(() => {
+        document.body.setAttribute('data-bs-theme', darkMode ? 'dark' : 'light');
+    }, [darkMode]);
+
     return (
         <Navbar expand={'lg'} className={'bg-body-tertiary'}>
             <Container>
                 <Navbar.Brand href={'/today'}>Pronóstico</Navbar.Brand>
                 <Navbar.Toggle aria-controls={'basic-navbar-nav'} />
-                <Navbar.Collapse id={'basic-navbar-nav'}>
+                <Navbar.Collapse>
                     <Nav className={'me-auto'}>
                         <Nav.Link href={'/today'}>Hoy</Nav.Link>
                         <Nav.Link href={'/extended'}>Extendido</Nav.Link>
                         <Nav.Link href={'/locations'}>Localidades</Nav.Link>
+                    </Nav>
+                    <Nav className={'ms-auto'}>
+                        <Nav.Link className='icon' href={''} onClick={toggleDarkMode}>
+                            {darkMode ? <Sun /> : <Moon />}
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
